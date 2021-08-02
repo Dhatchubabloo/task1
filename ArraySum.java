cbb4f9c36092a073ee20bcf9e81ee467f82e596d
Dhatchubabloo <69237176+Dhatchubabloo@users.noreply.github.com>
2021-08-02 19:42:20 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -7,13 +7,13 @@ public class ArraySum{
         int number = scan.nextInt();
         int [] display = new int[number];
         System.out.println("enter array values");
-        for(int i=0;i<n;i++){
+        for(int i=0;i<number;i++){
             int token = scan.nextInt();
             display[i] = token;
         }
         int sum = 0;
-        for(int i=0;i<n;i++){
-            sum = sum+a[i];
+        for(int i=0;i<number;i++){
+            sum = sum+display[i];
         }
         System.out.println("sum of the array elements is");
         System.out.println(sum);
